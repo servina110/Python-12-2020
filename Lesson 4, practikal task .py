abb42dfb5d5c6430805c4c45627c7ebc2f82c1f4
servina110 <76222148+servina110@users.noreply.github.com>
2021-01-02 10:31:43 +0300

    Update Lesson 4, practikal task .py

--- Lesson 4, practikal task .py
+++ Lesson 4, practikal task .py
@@ -23,7 +23,7 @@ sal()
 Результат: [12, 44, 4, 10, 78, 123]."""
 print("Task № 2")
 my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-my_new_list = [el for num, el in enumerate(my_list) if my_list[num - 1] < my_list[num]]
+my_new_list = [el for num, el in enumerate(my_list) if my_list[num - 1] < my_list[num] and num > 0]
 print(f'Исходный список: {my_list}')
 print(f'Результат: {my_new_list}')
 
